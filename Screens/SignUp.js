74f06c7232b490374bf694fbc36f4c58e323eee6
Abdullah2888 <18bscs29087@gmail.com>
2023-03-29 23:29:31 -0700

    Sign|UpNew

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -7,7 +7,7 @@ const SignUp = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
+    const handleLogin = () => { 
         auth()
             .createUserWithEmailAndPassword(email, password)
             .then(() => {
